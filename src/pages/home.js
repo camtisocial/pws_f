fa516094e62d85c747baa4e5bbb6321275f2f8ea
cameron <thompsonca99@gmail.com>
2024-11-15 16:26:08 -0600

    added basic setup for home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../css/home.css';
 import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <h1>HOMEPAGE</h1>
       <nav>
         <ul>
