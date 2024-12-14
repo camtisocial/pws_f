a2f6f1a237335067d0ee1b0d38ff68dc0b707407
cameron <thompsonca99@gmail.com>
2024-12-13 20:01:43 -0600

    monkey business

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../css/blog.css';
 import CardTilt from '../components/CardTilt';
-import frontMatter from 'front-matter';
+import fm from 'front-matter';
 import HomeButton from '../components/HomeButton';
 import { useNavigate } from 'react-router-dom';
 
@@ -10,54 +10,57 @@ import { useNavigate } from 'react-router-dom';
 function Blog() {
   const navigate = useNavigate();
   const [posts, setPosts] = useState([]);
-
   // useEffect(() => {
   //   fetch('lambdaLink')
   //     .then((res) => res.json())
-  //     // .then((data) => setPosts(JSON.parse(data)));
+  //     .then((data) => setPosts(JSON.parse(data)));
   // }, []);
   useEffect(() => {
   const mockData = [
     {
-      filename: 'first-post.mdx',
+      filename: 'FFfirst-post.mdx',
+      title: 'My First Blog Post',
+      date: '2024-12-01',
+      tags: ['React', 'MDX', 'AWS'],
       content: `
----
-title: "My First Blog Post"
-date: "2024-12-01"
-tags: ["React", "MDX", "AWS"]
----
 # My First Blog Post
 This is the content of the first post.
       `,
     },
+
     {
       filename: 'second-post.mdx',
+      title: 'My second Blog Post',
+      date: '2024-12-01',
+      tags: ['React', 'MDX', 'AWS'],
       content: `
----
-title: "Another Blog Post"
-date: "2024-12-08"
-tags: ["JavaScript", "S3", "Lambda"]
----
-# Another Blog Post
-This is the content of the second post.
+# My First Blog Post
+This is the content of the first post.
       `,
     },
+
   ];
 
-  const parsedPosts = mockData.map((post) => {
-    const { attributes: metadata, body: content } = frontMatter(post.content);
-    const { title, date, tags } = metadata; 
-    return {
-      filename: post.filename,
-      title,
-      date, 
-      tags: tags || [], 
-      content,
-    };
-  });
-
-  setPosts(parsedPosts);
+  setPosts(mockData);
 }, []);
+
+
+useEffect(() => {
+  const updateBlogListHeight = () => {
+    const blogList = document.querySelector('.blog-list');
+    const blogListHeight = blogList.scrollHeight;
+    blogList.style.setProperty('--blog-list-height', `${blogListHeight}px`);
+  };
+
+  updateBlogListHeight();
+
+  window.addEventListener('resize', updateBlogListHeight);
+
+  return () => {
+    window.removeEventListener('resize', updateBlogListHeight);
+  };
+}, [posts]);
+
 
 const handleCardClick = (filename) => {
   navigate(`/blog/${filename}`);
@@ -68,10 +71,13 @@ const handleCardClick = (filename) => {
       <div className="blog-list">
         {posts.map((post) => (
           <CardTilt key={post.filename}>
+            {/* <div className="blog-card" key={post.filename} onClick={() => handleCardClick(post.filename)} style={{ '--title-date': `"${post.date}"` }}> */}
             <div className="blog-card" key={post.filename} onClick={() => handleCardClick(post.filename)}>
-              <h2>{post.title}</h2>
-              <p>{post.date}</p>
-              <p>{post.tags.join(', ')}</p>
+              <div className = "blog-card-content">
+                <h2>{post.title}</h2>
+                <p>{post.date}</p>
+                <p>{post.tags.join(', ')}</p>
+              </div>
             </div>
           </CardTilt>
         ))}
