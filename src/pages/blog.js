3622833f4819bf41768e342d7161c2ac8b9592d7
cameron <thompsonca99@gmail.com>
2024-12-13 20:21:50 -0600

    tweaks to blog cards

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -22,6 +22,7 @@ function Blog() {
       title: 'My First Blog Post',
       date: '2024-12-01',
       tags: ['React', 'MDX', 'AWS'],
+      backgroundImageUrl: 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2tybXczMGo1bDJrMXpwZTltMGhtbzJoOTdhMXNhYThjYWtmemVoYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/NoGhgVB9ybIJ2/giphy.webp',
       content: `
 # My First Blog Post
 This is the content of the first post.
@@ -33,6 +34,7 @@ This is the content of the first post.
       title: 'My second Blog Post',
       date: '2024-12-01',
       tags: ['React', 'MDX', 'AWS'],
+      backgroundImageUrl: 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2pjeXNpMWZrdmR5NGlxNG5wdW13YjJjZmk1dHE5MTg5Z2t1eTVnMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Rlwz4m0aHgXH13jyrE/giphy.webp',
       content: `
 # My First Blog Post
 This is the content of the first post.
@@ -72,11 +74,18 @@ const handleCardClick = (filename) => {
         {posts.map((post) => (
           <CardTilt key={post.filename}>
             {/* <div className="blog-card" key={post.filename} onClick={() => handleCardClick(post.filename)} style={{ '--title-date': `"${post.date}"` }}> */}
-            <div className="blog-card" key={post.filename} onClick={() => handleCardClick(post.filename)}>
-              <div className = "blog-card-content">
-                <h2>{post.title}</h2>
-                <p>{post.date}</p>
-                <p>{post.tags.join(', ')}</p>
+            <div
+             className="blog-card"
+             key={post.filename} 
+             onClick={() => handleCardClick(post.filename)}>
+              <div 
+              className="blog-card-image"
+              style={{backgroundImage: `url(${post.backgroundImageUrl})`}}>
+                 <div className = "blog-card-content">
+                   <h2>{post.title}</h2>
+                   <p>{post.date}</p>
+                   <p>{post.tags.join(', ')}</p>
+                 </div>
               </div>
             </div>
           </CardTilt>
